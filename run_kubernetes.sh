cfdd4c97d21c8cd14805d9d5db9f1131eda703a8
t.benedek@web.de <t.beneden@web.de>
2021-06-05 17:37:55 +0200

    testing new circleci config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-kubectl delete deployment quarkus-hello
-
-# Step 1:
-# This is your Docker ID/path
+# define variables
 dockerpath=tbenedek/quarkus-hello
+deplname = quarkus-hello
+
+# This tags and uploads an image to Docker Hub
+kubectl delete deployment $deplname
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment quarkus-hello --image=$dockerpath --port=8080
+kubectl create deployment $deplname --image=$dockerpath --port=8080
 
 # Step 3:
 # sleep for 15 seconds to wait for pod to come up
 sleep 15
 
-# show pods
-kubectl get pods
-
-# Step 4:
 # Forward the container port to a host
-# kubectl port-forward pod/ml-housing 8000:8000
-
-kubectl port-forward deployment/quarkus-hello 8080:80
+kubectl port-forward deployment/$deplname 8080:80
