dda454e25655714d2aa9b2a193a6398794aec1f8
t.benedek@web.de <t.beneden@web.de>
2021-06-06 18:26:51 +0200

    test

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 docker build --tag=quarkus-hello .
 
-docker run -it -p 8080:80 quarkus-hello
+docker run -it -p 80:80 quarkus-hello
 
 # Try to run for quarkus example
 # docker run -it -p8000:8000 --rm quarkus-hello "./mvnw quarkus:dev:"
