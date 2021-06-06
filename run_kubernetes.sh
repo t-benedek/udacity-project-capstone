8a9bd7ed79a11263241239c0402f3370fb0de1a1
t.benedek@web.de <t.beneden@web.de>
2021-06-06 15:04:20 +0200

    added kubernetes service file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,18 +2,17 @@
 
 # define variables
 dockerpath=tbenedek/quarkus-hello
-deplname = quarkus-hello
 
 # This tags and uploads an image to Docker Hub
-kubectl delete deployment $deplname
+kubectl delete deployment quarkus-hello
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment $deplname --image=$dockerpath --port=8080
+kubectl create deployment quarkus-hello --image=$dockerpath --port=8080
 
 # Step 3:
 # sleep for 15 seconds to wait for pod to come up
 sleep 15
 
 # Forward the container port to a host
-kubectl port-forward deployment/$deplname 8080:80
+kubectl port-forward deployment/quarkus-hello 8080:80
